7f27410ba9ec2ddec2e046a0520717e1921b219d
gitkenbar <kennethbarker12@gmail.com>
2024-03-11 15:13:59 -0500

    Might wanna publish this

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+  path: "",
+  pathMatch: "full",
+  loadComponent: () => import("./features/timeline/timeline.component").then((c) => c.TimelineComponent)
+},
+{
+  path: 'events',
+  loadComponent: ()=> import("./features/events/events.component").then((c) => c.EventsComponent)
+},
+{
+  path: 'login',
+  loadComponent: ()=> import("./features/auth/login/login.component").then((c) => c.LoginComponent)
+}
+];
