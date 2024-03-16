5d6c74faf4db1c4396cd6dbd34742782c8c22ba9
gitkenbar <kennethbarker12@gmail.com>
2024-03-16 16:26:47 -0500

    Gather user info on app initialization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,32 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { UserService } from './core/services/user.service';
+import { AuthenticationService } from './core/services/authentication.service';
+import { of } from 'rxjs';
+
+export function initializedUserData(
+  userService:UserService,
+  authService:AuthenticationService
+  ){
+    console.log("THIS executed");
+  if(authService.isLoggedIn()){
+    return () => userService.getBootstrapData().subscribe()
+  }else {
+    return () => of(null);
+  }
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializedUserData,
+      deps: [UserService, AuthenticationService],
+      multi: true
+    },
+    provideHttpClient()]
 };
